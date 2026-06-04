023b3250799a09ed210ade65e4f3166cab33a6bd
navarrs <ingrid.navarro.an@gmail.com>
2026-06-04 10:49:05 -0400

    Added naive learned models

--- run_distribution_shift_sweep.sh
+++ run_distribution_shift_sweep.sh
@@ -10,7 +10,7 @@ Usage: $0 [options]
 
 Options:
   -m <models>       Model(s), comma-separated
-                    (default: wayformer, scenetransformer, autobot, mtr, cvm)
+                    (default: wayformer, scenetransformer, autobot, mtr, naive, cvm)
   -d <devices>      Devices (e.g. 0 or 0,1)
                     (default: 0)
   -b <benchmarks>   Benchmark(s), comma-separated
@@ -47,6 +47,7 @@ DEFAULT_MODELS=(
     scenetransformer
     autobot
     mtr
+    naive
     cvm
 )
 DEFAULT_DEVICES="0"
